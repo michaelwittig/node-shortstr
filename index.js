752e92c761bf8d057d280c16635d74e5865a7b7e
Michael Wittig <michael.wittig@tullius-walden.com>
2014-11-07 08:55:53 +0100

    tests passing

--- index.js
+++ index.js
@@ -1,58 +1,91 @@
 var assert = require("assert-plus");
-var BitString = require("bitstring");
+var bb = require("bit-buffer");
 
-function Shortener(from, to) {
+function createZeroBuffer(size) {
+	"use strict";
+	var buffer = new Buffer(size);
+	buffer.fill(0);
+	return buffer;
+}
+function bits2bytes(bits) {
+	"use strict";
+	return Math.ceil(bits/8);
+}
+
+function Shortener(from, to, fromMaxLength) {
 	"use strict";
 	this.fromCharacters = from;
 	this.fromBitsNeeded = Math.ceil(Math.log(from.length)/Math.log(2));
+	this.fromMaxLength = fromMaxLength;
+	this.fromMaxLengthBits = 1 + Math.ceil(Math.log(fromMaxLength)/Math.log(2));
 	this.toCharacters = to;
-	this.toBitsNeeded = Math.ceil(Math.log(to.length)/Math.log(2));
+	this.toBitsNeeded = Math.floor(Math.log(to.length)/Math.log(2));
 }
 Shortener.prototype.shorten = function(from) {
 	"use strict";
 	assert.string(from, "from");
-	var f = new BitString(), i, fromi, toi, to = "";
+	if (from.length > this.fromMaxLength) {
+		throw new Error("violates fromMaxLength");
+	}
+	var fromBits = this.fromMaxLengthBits + (from.length*this.fromBitsNeeded),
+		toBitsToRead = fromBits,
+		buffer = createZeroBuffer(bits2bytes(fromBits)),
+		bv = new bb.BitView(buffer),
+		bs = new bb.BitStream(bv), i, fromi, toi, to = "";
+	bs.writeBits(from.length, this.fromMaxLengthBits);
 	for (i = 0; i < from.length; i += 1) {
 		fromi = this.fromCharacters.indexOf(from[i]);
 		if (fromi === -1) {
 			throw new Error("out of from");
 		}
-		f.writebits(fromi, this.fromBitsNeeded);
+		bs.writeBits(fromi, this.fromBitsNeeded);
 	}
-	f.flush(); f._pos = 0; // reset position to beginning
-	for (i = 0; i < from.length*this.fromBitsNeeded; i+= this.toBitsNeeded) {
-		toi = f.readbits(this.toBitsNeeded);
+	bs = new bb.BitStream(bv);
+	for (i = 0; i < fromBits; i+= this.toBitsNeeded) {
+		toi = bs.readBits(Math.min(this.toBitsNeeded, toBitsToRead), false);
 		to += this.toCharacters[toi];
+		toBitsToRead -= this.toBitsNeeded;
 	}
 	return to;
 };
 Shortener.prototype.expand = function(to) {
 	"use strict";
 	assert.string(to, "to");
-	var f = new BitString(), i, toi, fromi, from = "";
+	var toBits = this.fromMaxLengthBits + (to.length*this.toBitsNeeded),
+		fromBitsToRead = toBits,
+		buffer = createZeroBuffer(toBits),// how long?
+		bv = new bb.BitView(buffer),
+		bs = new bb.BitStream(bv), i, toi, fromi, from = "", fromLength;
 	for (i = 0; i < to.length; i += 1) {
 		toi = this.toCharacters.indexOf(to[i]);
 		if (toi === -1) {
 			throw new Error("out of to");
 		}
-		f.writebits(toi, this.toBitsNeeded);
+		bs.writeBits(toi, this.toBitsNeeded);
 	}
-	f.flush(); f._pos = 0; // reset position to beginning
-	for (i = 0; i < to.length*this.toBitsNeeded; i+= this.fromBitsNeeded) {
-		fromi = f.readbits(this.fromBitsNeeded);
+	bs = new bb.BitStream(bv);
+	fromLength = bs.readBits(this.fromMaxLengthBits, false);
+	for (i = 0; i < fromLength*this.fromBitsNeeded; i+= this.fromBitsNeeded) {
+		fromi = bs.readBits(Math.min(this.fromBitsNeeded, fromBitsToRead), false);
 		from += this.fromCharacters[fromi];
+		fromBitsToRead -= this.fromBitsNeeded;
 	}
 	return from;
 };
-exports.create = function(from, to) {
+exports.create = function(from, to, fromMaxLength) {
 	"use strict";
 	assert.string(from, "from");
 	assert.string(to, "to");
+	assert.optionalNumber(fromMaxLength, "fromMaxLength");
+	fromMaxLength = fromMaxLength || 64;
 	if (from.length < 2) {
 		throw new Error("from must have at least 2 characters");
+	}
+	if (from.length === to.length) {
+		throw new Error("from and to have same amount of chars");
 	}
 	if (from.length > to.length) {
 		throw new Error("from has more chars than to");
 	}
-	return new Shortener(from, to);
+	return new Shortener(from, to, fromMaxLength);
 };
--- test/shortener.js
+++ test/shortener.js
@@ -4,28 +4,76 @@ var shortstr = require("../index.js");
 
 describe("shortstr", function() {
 	"use strict";
-	it("2 => 2", function() {
-		var shortener = shortstr.create("12", "ab");
-		assert.equal(shortener.expand(shortener.shorten("1221")), "1221");
-	});
 	it("2 => 4", function() {
-		var shortener = shortstr.create("12", "abcd");
-		assert.equal(shortener.expand(shortener.shorten("1221")), "1221");
+		var str = "1221",
+			shortener = shortstr.create("12", "abcd", 4),
+			sho = shortener.shorten(str);
+		console.log(str + " => " + sho);
+		assert.ok(sho.length <= str.length);
+		assert.equal(shortener.expand(sho), str);
 	});
 	it("2 => 8", function() {
-		var shortener = shortstr.create("12", "abcdefgh");
-		assert.equal(shortener.expand(shortener.shorten("1221")), "1221");
+		var str = "1221",
+			shortener = shortstr.create("12", "abcdefgh", 4),
+			sho = shortener.shorten(str);
+		console.log(str + " => " + sho);
+		assert.ok(sho.length <= str.length);
+		assert.equal(shortener.expand(sho), str);
 	});
 	it("2 => 16", function() {
-		var shortener = shortstr.create("12", "abcdefghijklmnop");
-		assert.equal(shortener.expand(shortener.shorten("1221")), "1221");
+		var str = "1221",
+			shortener = shortstr.create("12", "abcdefghijklmnop", 4),
+			sho = shortener.shorten(str);
+		console.log(str + " => " + sho);
+		assert.ok(sho.length <= str.length);
+		assert.equal(shortener.expand(sho), str);
+	});
+	it("2 => 32", function() {
+		var str = "1221",
+			shortener = shortstr.create("12", "abcdefghijklmnopABCDEFGHIJKLMNOP", 4),
+			sho = shortener.shorten(str);
+		console.log(str + " => " + sho);
+		assert.ok(sho.length <= str.length);
+		assert.equal(shortener.expand(sho), str);
+	});
+	it("4 => 8", function() {
+		var str = "12344321",
+			shortener = shortstr.create("1234", "abcdefgh", 16),
+			sho = shortener.shorten(str);
+		console.log(str + " => " + sho);
+		assert.ok(sho.length <= str.length);
+		assert.equal(shortener.expand(sho), str);
 	});
 	it("4 => 16", function() {
-		var shortener = shortstr.create("1234", "abcdefghijklmnop");
-		assert.equal(shortener.expand(shortener.shorten("12344321")), "12344321");
+		var str = "12344321",
+			shortener = shortstr.create("1234", "abcdefghijklmnop", 16),
+			sho = shortener.shorten(str);
+		console.log(str + " => " + sho);
+		assert.ok(sho.length <= str.length);
+		assert.equal(shortener.expand(sho), str);
+	});
+	it("4 => 32", function() {
+		var str = "12344321",
+			shortener = shortstr.create("1234", "abcdefghijklmnopABCDEFGHIJKLMNOP", 16),
+			sho = shortener.shorten(str);
+		console.log(str + " => " + sho);
+		assert.ok(sho.length <= str.length);
+		assert.equal(shortener.expand(sho), str);
 	});
 	it("8 => 16", function() {
-		var shortener = shortstr.create("12345678", "abcdefghijklmnop");
-		assert.equal(shortener.expand(shortener.shorten("1234567887654321")), "1234567887654321");
+		var str = "1234567887654321",
+			shortener = shortstr.create("12345678", "abcdefghijklmnop", 16),
+			sho = shortener.shorten(str);
+		console.log(str + " => " + sho);
+		assert.ok(sho.length <= str.length);
+		assert.equal(shortener.expand(sho), str);
+	});
+	it("8 => 32", function() {
+		var str = "1234567887654321",
+			shortener = shortstr.create("12345678", "abcdefghijklmnopABCDEFGHIJKLMNOP", 16),
+			sho = shortener.shorten(str);
+		console.log(str + " => " + sho);
+		assert.ok(sho.length <= str.length);
+		assert.equal(shortener.expand(sho), str);
 	});
 });
